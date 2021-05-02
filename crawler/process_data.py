e51251ef666e318a1b240e1b7a80b556486e2c10
Christoph Saffer <Christoph.Saffer@hki-jena.de>
2021-05-02 11:30:28 +0200

    add weekday in merged datasets

--- crawler/process_data.py
+++ crawler/process_data.py
@@ -19,15 +19,19 @@ def get_files_last_N_days(N):
     return np.sort([d for d in get_csv_files(DIR_DATA) if datetime.datetime.strptime(d.split(".")[0], '%Y-%m-%d') > dt])
 
 
-def get_data_last_N_days(N):
+def get_data_last_N_days(N, step_size=0):
     files = get_files_last_N_days(N)
-    step_size = len(files)
+    if step_size == 0:
+        step_size = len(files)
     df_combined = pd.DataFrame()
     for day in files:
         df = pd.read_csv("{}/{}".format(DIR_DATA, day), index_col=0)
         df = df[df.index % step_size == 0]
+        df["timestamp"] = [timestamp.split(".")[0] for timestamp in df["timestamp"]]
         df.insert(0, "day", day.split(".")[0])
+        df.insert(0, "weekday", datetime.datetime.strptime(day.split(".")[0], '%Y-%m-%d').weekday())
         df_combined = df_combined.append(df)
+    df_combined.set_index(np.arange(len(df_combined)), inplace=True)
 
     return df_combined
 
